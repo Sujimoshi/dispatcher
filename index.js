e030aa2a745f8b7a0dcf689218407451b371164f
Igor Solomakha <igor.solomakha@gmail.com>
2022-03-15 19:15:48 +0100

    update

---
+++ index.js
@@ -0,0 +1,39 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+;(async function() {
+  const owner = core.getInput('owner');
+  const repo = core.getInput('repo');
+  const ref = core.getInput('ref');
+  const workflow = core.getInput('workflow');
+  const token = core.getInput('token');
+  const payload = JSON.parse(core.getInput('payload') || '{}');
+  const marker_input_name = core.getInput('marker_input_name') || 'caller'
+
+  const octokit = github.getOctokit(token);
+
+  await octokit.rest.actions.createWorkflowDispatch({
+    owner, repo, ref, workflow_id: workflow,
+    inputs: { 
+      [marker_input_name]: `https://github.com/${github.context.owner}/${github.context.repo}/actions/runs/${github.context.runId}`,
+      ...Object.entries(payload).reduce((acc, [key, value]) => ({ ...acc, [key]: JSON.stringify(value) }), {})
+    }
+  })
+  
+  
+  const { data: { workflow_runs } } = await octokit.rest.actions.listWorkflowRuns({
+    owner, repo, branch: ref, workflow_id: workflow, event: 'workflow_dispatch'
+  })
+
+  let desired_run
+  for (const run of workflow_runs) {
+    const { data: { jobs } } = await octokit.rest.actions.listJobsForWorkflowRun({
+      owner, repo, run_id: run.id, filter: 'latest'
+    })
+
+    for (const job of jobs) {
+      console.log(job.name, job.steps)
+    }
+  }
+
+})().catch(err => core.setFailed(err.message))
