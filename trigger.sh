137f4baab4d14f7f83292c9e6e461dad3a184074
Igor Solomakha <igor.solomakha@gmail.com>
2022-03-16 22:06:00 +0100

    fix: update

--- trigger.sh
+++ trigger.sh
@@ -20,7 +20,7 @@ CONFIG=`node -e "console.log(JSON.stringify({
 
 echo "$CONFIG" | gh workflow run "$WORKFLOW" --json --ref "$REF" -R "$REPO"
 
-for i in {1..30}; do
+for i in `seq 1 30`; do
   echo $i
   NEW_RUNS=$(gh run list -w $WORKFLOW -R $REPO --json databaseId -q '.[].databaseId')
   echo "OLD_RUNS: '$OLD_RUNS'"
@@ -28,19 +28,7 @@ for i in {1..30}; do
   DIFF_RUNS=`for RID in $NEW_RUNS; do echo $OLD_RUNS | grep -q $RID || echo $RID; done`
   echo "DIFF_RUNS: '$DIFF_RUNS'"
   RUN=`for RUN_ID in $DIFF_RUNS; do gh run view "$RUN_ID" -R "$REPO" -v | grep -q "$CALLER" && echo "$RUN_ID"; done`
-  # if [[ "$RUN" != ''  ]]; then 
-  #   echo 'break';
-  #   break;
-  # else
-  #   echo 'sleep';
-  #   sleep 3;
-  # fi
-done
-
-
-for i in {1..30}; do
-  echo $i
-  sleep 5
+  [[ "$RUN" != ''  ]] && break; || sleep 5
 done
 
 echo "Watching for https://github.com/$REPO/actions/runs/$RUN"
