cf9e2de20b35fada1b891a0366d818174c49a548
Igor Solomakha <igor.solomakha@gmail.com>
2022-03-16 21:53:14 +0100

    fix: update

--- trigger.sh
+++ trigger.sh
@@ -24,9 +24,9 @@ for i in {1..30}; do
   NEW_RUNS=$(gh run list -w $WORKFLOW -R $REPO --json databaseId -q '.[].databaseId')
   echo "OLD_RUNS: '$OLD_RUNS'"
   echo "NEW_RUNS: '$NEW_RUNS'"
-  DIFF_RUNS=$(for RID in $NEW_RUNS; do echo $OLD_RUNS | grep -q $RID || echo $RID; done)
+  DIFF_RUNS=`for RID in $NEW_RUNS; do echo $OLD_RUNS | grep -q $RID || echo $RID; done`
   echo "DIFF_RUNS: '$DIFF_RUNS'"
-  RUN=$(for RUN_ID in $DIFF_RUNS; do gh run view $RUN_ID -R $REPO -v | grep -q $CALLER && echo $RUN_ID; done)
+  RUN=`for RUN_ID in $DIFF_RUNS; do gh run view $RUN_ID -R $REPO -v | grep -q $CALLER && echo $RUN_ID; done`
   # if [[ "$RUN" != ''  ]]; then 
   #   echo 'break';
   #   break;
@@ -38,4 +38,4 @@ done
 
 echo "Watching for https://github.com/$REPO/actions/runs/$RUN"
 
-# gh run watch -R $REPO --exit-status $RUN
+gh run watch -R $REPO --exit-status $RUN
