ee2dc8c1be132bad7789e9ca92385e5fd3a98abb
Igor Solomakha <igor.solomakha@gmail.com>
2022-03-16 21:26:18 +0100

    fix: update

--- trigger.sh
+++ trigger.sh
@@ -25,7 +25,7 @@ for i in {1..30}; do
   DIFF_RUNS=$(for i in $NEW_RUNS; do echo $OLD_RUNS | grep -q $i || echo $i; done)
   echo "DIFF_RUNS: '$DIFF_RUNS'"
   RUN=$(for RUN_ID in $DIFF_RUNS; do gh run view $RUN_ID -R $REPO -v | grep -q $CALLER && echo $RUN_ID; done)
-  echo "RUN: '$DIFF_RUNS'"
+  echo "RUN: '$DIFF_RUNS' '$([[ "$RUN" != ''  ]] && echo 'break' || echo 'sleep')'"
   [[ "$RUN" != ''  ]] && break || sleep 3
 done
 
